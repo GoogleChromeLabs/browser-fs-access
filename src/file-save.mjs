798df73654878160d24036eac1abf253d16e58b4
Jason Miller <developit@users.noreply.github.com>
2020-01-23 10:36:41 -0500

    window -> self

--- src/file-save.mjs
+++ src/file-save.mjs
@@ -15,7 +15,7 @@
  */
 // @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
 
-const implementation = 'chooseFileSystemEntries' in window
+const implementation = 'chooseFileSystemEntries' in self
   ? import('./file-save-nativefs.mjs')
   : import('./file-save-legacy.mjs');
 
