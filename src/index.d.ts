1160b12bd7f95e282c6f6bc4b66d2ca69d41f594
Alex Bates <hi@imalex.xyz>
2020-05-05 00:06:37 +0100

    Add FileSystemHandle interface

--- src/index.d.ts
+++ src/index.d.ts
@@ -24,3 +24,22 @@ export interface DirectoryOpenOptions {
 export function directoryOpen(options?: DirectoryOpenOptions): Promise<File[]>;
 
 export function imageToBlob(img: HTMLImageElement): Promise<Blob>;
+
+// The following typings implement the relevant parts of the Native File System API.
+// This can be removed once the specification reaches the Candidate phase and is
+// implemented as part of microsoft/TSJS-lib-generator.
+
+export interface FileSystemHandlePermissionDescriptor {
+  writable: boolean,
+}
+
+export interface FileSystemHandle {
+  readonly isFile: boolean,
+  readonly isDirectory: boolean,
+  readonly name: string,
+
+  isSameEntry: (other: FileSystemHandle) => Promise<boolean>,
+
+  queryPermission: (descriptor?: FileSystemHandlePermissionDescriptor) => Promise<PermissionState>,
+  requestPermission: (descriptor?: FileSystemHandlePermissionDescriptor) => Promise<PermissionState>,
+}
