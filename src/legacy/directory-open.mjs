GoogleChromeLabs/browser-fs-access

commit b6e855dc0cf46afb3e6faf2be465dbb8e7ec37e3

Use `cancel` event Fixes #153 Fixes #158

src/legacy/directory-open.mjs
@@ -30,18 +30,10 @@ export default async (options = [{}]) => {
     input.type = 'file';
     input.webkitdirectory = true;
 
-    const _reject = () => cleanupListenersAndMaybeReject(reject);
-    const _resolve = (value) => {
-      if (typeof cleanupListenersAndMaybeReject === 'function') {
-        cleanupListenersAndMaybeReject();
-      }
-      resolve(value);
-    };
-    // ToDo: Remove this workaround once
-    // https://github.com/whatwg/html/issues/6376 is specified and supported.
-    const cleanupListenersAndMaybeReject =
-      options[0].legacySetup &&
-      options[0].legacySetup(_resolve, _reject, input);
+    input.addEventListener('cancel', () => {
+      input.remove();
+      reject(new DOMException('The user aborted a request.', 'AbortError'));
+    });
 
     input.addEventListener('change', () => {
       let files = Array.from(input.files);
@@ -62,7 +54,7 @@ export default async (options = [{}]) => {
         });
       }
 
-      _resolve(files);
+      resolve(files);
     });
     if ('showPicker' in HTMLInputElement.prototype) {
       input.showPicker();

src/legacy/file-open.mjs
@@ -44,19 +44,10 @@ export default async (options = [{}]) => {
       }
       resolve(value);
     };
-    // ToDo: Remove this workaround once
-    // https://github.com/whatwg/html/issues/6376 is specified and supported.
-    const cleanupListenersAndMaybeReject =
-      options[0].legacySetup &&
-      options[0].legacySetup(_resolve, _reject, input);
 
-    const cancelDetector = () => {
-      window.removeEventListener('focus', cancelDetector);
+    input.addEventListener('cancel', () => {
       input.remove();
-    };
-
-    input.addEventListener('click', () => {
-      window.addEventListener('focus', cancelDetector);
+      reject(new DOMException('The user aborted a request.', 'AbortError'));
     });
 
     input.addEventListener('change', () => {

eslint.config.mjs
@@ -1,0 +1,23 @@
+// eslint.config.js
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['**/*.js', '**/*.mjs'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    rules: {
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      indent: ['error', 2],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'comma-dangle': ['error', 'never'],
+      'require-jsdoc': 'off',
+      'valid-jsdoc': 'off',
+      ...prettierConfig.rules,
+    },
+  },
+];

index.d.ts
@@ -53,9 +53,6 @@ export interface FirstFileSaveOptions extends FirstCoreFileOptions {
    *     };
    *   },
    * });
-   *
-   * ToDo: Remove this workaround once
-   *   https://github.com/whatwg/html/issues/6376 is specified and supported.
    */
   legacySetup?: (
     resolve: () => void,
@@ -67,48 +64,12 @@ export interface FirstFileSaveOptions extends FirstCoreFileOptions {
 /**
  * The first `options` object passed to file open operations can specify
  * whether multiple files can be selected (the return type of the operation
- * will be updated appropriately) and a way of handling cleanup and rejection
- * for legacy open operations.
+ * will be updated appropriately).
  */
 export interface FirstFileOpenOptions<M extends boolean | undefined>
   extends FirstCoreFileOptions {
   /** Allow multiple files to be selected. Defaults to `false`. */
   multiple?: M;
-  /**
-   * Configurable cleanup and `Promise` rejector usable with legacy API for
-   * determining when (and reacting if) a user cancels the operation. The
-   * method will be passed a reference to the internal `rejectionHandler` that
-   * can, e.g., be attached to/removed from the window or called after a
-   * timeout. The method should return a function that will be called when
-   * either the user chooses to open a file or the `rejectionHandler` is
-   * called. In the latter case, the returned function will also be passed a
-   * reference to the `reject` callback for the `Promise` returned by
-   * `fileOpen`, so that developers may reject the `Promise` when desired at
-   * that time.
-   * Example rejector:
-   *
-   * const file = await fileOpen({
-   *   legacySetup: (rejectionHandler) => {
-   *     const timeoutId = setTimeout(rejectionHandler, 10_000);
-   *     return (reject) => {
-   *       clearTimeout(timeoutId);
-   *       if (reject) {
-   *         reject('My error message here.');
-   *       }
-   *     };
-   *   },
-   * });
-   *
-   * ToDo: Remove this workaround once
-   *   https://github.com/whatwg/html/issues/6376 is specified and supported.
-   */
-  legacySetup?: (
-    resolve: (
-      value: M extends false | undefined ? FileWithHandle : FileWithHandle[]
-    ) => void,
-    rejectionHandler: () => void,
-    input: HTMLInputElement
-  ) => (reject?: (reason?: any) => void) => void;
 }
 
 /**
@@ -174,39 +135,6 @@ export function directoryOpen(options?: {
   skipDirectory?: (
     entry: FileSystemDirectoryEntry | FileSystemDirectoryHandle
   ) => boolean;
-  /**
-   * Configurable setup, cleanup and `Promise` rejector usable with legacy API
-   * for determining when (and reacting if) a user cancels the operation. The
-   * method will be passed a reference to the internal `rejectionHandler` that
-   * can, e.g., be attached to/removed from the window or called after a
-   * timeout. The method should return a function that will be called when
-   * either the user chooses to open a file or the `rejectionHandler` is
-   * called. In the latter case, the returned function will also be passed a
-   * reference to the `reject` callback for the `Promise` returned by
-   * `fileOpen`, so that developers may reject the `Promise` when desired at
-   * that time.
-   * Example rejector:
-   *
-   * const file = await directoryOpen({
-   *   legacySetup: (rejectionHandler) => {
-   *     const timeoutId = setTimeout(rejectionHandler, 10_000);
-   *     return (reject) => {
-   *       clearTimeout(timeoutId);
-   *       if (reject) {
-   *         reject('My error message here.');
-   *       }
-   *     };
-   *   },
-   * });
-   *
-   * ToDo: Remove this workaround once
-   *   https://github.com/whatwg/html/issues/6376 is specified and supported.
-   */
-  legacySetup?: (
-    resolve: (value: FileWithDirectoryAndFileHandle) => void,
-    rejectionHandler: () => void,
-    input: HTMLInputElement
-  ) => (reject?: (reason?: any) => void) => void;
 }): Promise<FileWithDirectoryAndFileHandle[] | FileSystemDirectoryHandle[]>;
 
 /**

src/legacy/file-save.mjs
@@ -41,8 +41,7 @@ export default async (blobOrPromiseBlobOrResponse, options = {}) => {
       cleanupListenersAndMaybeReject();
     }
   };
-  // ToDo: Remove this workaround once
-  // https://github.com/whatwg/html/issues/6376 is specified and supported.
+
   const cleanupListenersAndMaybeReject =
     options.legacySetup && options.legacySetup(_resolve, _reject, a);
 

commit 669637fdf59c4a39bb0721a547b6de739e8943f5

Fix issues introduced with https://github.com/AimWhy/browser-fs-access/commit/b6e855dc0cf46afb3e6faf2be465dbb8e7ec37e3

src/legacy/directory-open.mjs
@@ -29,6 +29,10 @@ export default async (options = [{}]) => {
     const input = document.createElement('input');
     input.type = 'file';
     input.webkitdirectory = true;
+    // Append to the DOM, else Safari on iOS won't fire the `change` event
+    // reliably.
+    input.style.display = 'none';
+    document.body.append(input);
 
     input.addEventListener('cancel', () => {
       input.remove();
@@ -36,6 +40,7 @@ export default async (options = [{}]) => {
     });
 
     input.addEventListener('change', () => {
+      input.remove();
       let files = Array.from(input.files);
       if (!options[0].recursive) {
         files = files.filter((file) => {

src/legacy/file-open.mjs
@@ -37,13 +37,6 @@ export default async (options = [{}]) => {
     // reliably.
     input.style.display = 'none';
     document.body.append(input);
-    const _reject = () => cleanupListenersAndMaybeReject(reject);
-    const _resolve = (value) => {
-      if (typeof cleanupListenersAndMaybeReject === 'function') {
-        cleanupListenersAndMaybeReject();
-      }
-      resolve(value);
-    };
 
     input.addEventListener('cancel', () => {
       input.remove();
@@ -51,9 +44,8 @@ export default async (options = [{}]) => {
     });
 
     input.addEventListener('change', () => {
-      window.removeEventListener('focus', cancelDetector);
       input.remove();
-      _resolve(input.multiple ? Array.from(input.files) : input.files[0]);
+      resolve(input.multiple ? Array.from(input.files) : input.files[0]);
     });
 
     if ('showPicker' in HTMLInputElement.prototype) {